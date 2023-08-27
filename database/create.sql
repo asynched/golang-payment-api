b098ea1e3615d0894b423aafa86970255630d27c
Eder Lima <devilsouls666@gmail.com>
2023-08-27 02:08:25 -0300

    feat: update database schema to add transactions table

--- database/create.sql
+++ database/create.sql
@@ -1,3 +1,11 @@
+/*
+ - Note:
+ The balance is represented as an int because of floating point arithmetic.
+ To cast the balance to a float, divide it by 100. Every user starts with 10000 cents (100.00).
+ 
+ - Note:
+ To apply the schema to the database, simply run: cat database/create.sql | sqlite3 database.sqlite
+ */
 -- Create schema
 CREATE TABLE IF NOT EXISTS "users" (
   id INTEGER PRIMARY KEY AUTOINCREMENT,
@@ -5,7 +13,18 @@ CREATE TABLE IF NOT EXISTS "users" (
   email VARCHAR(255) NOT NULL UNIQUE,
   cpf VARCHAR(11) NOT NULL UNIQUE,
   password VARCHAR(255) NOT NULL,
+  balance INTEGER NOT NULL DEFAULT 10000,
   created_at DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP
+);
+
+CREATE TABLE IF NOT EXISTS "transactions" (
+  id INTEGER PRIMARY KEY AUTOINCREMENT,
+  payer_id INTEGER NOT NULL,
+  payee_id INTEGER NOT NULL,
+  value INTEGER NOT NULL,
+  created_at DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  FOREIGN KEY (payer_id) REFERENCES users(id),
+  FOREIGN KEY (payee_id) REFERENCES users(id)
 );
 
 -- Add indexes
